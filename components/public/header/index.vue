2368a2465062222a26df5b7731f72fc5a65d30f9
qin-ziqi <348633040@qq.com>
2020-10-26 10:28:50 +0800

    增加assets中的css

---
+++ components/public/header/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row class="m-header" :gutter="15">
+    <el-col :span="6">
+      <geo />
+    </el-col>
+    <!-- <el-col :span="6">
+      <user />
+    </el-col>
+    <el-col :span="6"></el-col> -->
+  </el-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Geo from "./geo.vue";
+import User from "./user.vue";
+
+@Component({
+  components: {
+    Geo
+  }
+})
+export default class MyHeader extends Vue {}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/public/layout.scss";
+</style>
